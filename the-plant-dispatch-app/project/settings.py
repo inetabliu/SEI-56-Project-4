b1f20fa3189c307681b77c1ea2a529ddee6b8768
Your Name <ineta.bliu@gmail.com>
2021-07-21 21:59:19 +0100

    user model created and amdin superuser is working

--- the-plant-dispatch-app/project/settings.py
+++ the-plant-dispatch-app/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jwt_auth'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'the-plant-dispatch',
+        'HOST': 'localhost',
+        'POST': 5432
     }
 }
 
@@ -122,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'jwt_auth.User'
