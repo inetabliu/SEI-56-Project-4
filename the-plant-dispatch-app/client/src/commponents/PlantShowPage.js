d3d9c9726e5ad0e12515f300c8c3a16e00ad5aa1
Your Name <ineta.bliu@gmail.com>
2021-07-22 17:54:57 +0100

    plant data displaying

--- the-plant-dispatch-app/client/src/commponents/PlantShowPage.js
+++ the-plant-dispatch-app/client/src/commponents/PlantShowPage.js
@@ -1,7 +1,20 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 const PlantShowPage = () => {
 
+  const [plantShow, setPlantShow] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get('/api/plants/')
+      setPlantShow(data)
+      console.log('my plants',data)
+    }
+    getData()
+  }, [])
 
   return (
     <p>Page where my plants live</p>
