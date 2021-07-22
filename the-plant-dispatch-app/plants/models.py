9e9c7d51a95792a4f67b4049967ef7ed61033bef
Your Name <ineta.bliu@gmail.com>
2021-07-22 17:49:19 +0100

    plant update delete and edit function working on detail view

--- the-plant-dispatch-app/plants/models.py
+++ the-plant-dispatch-app/plants/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Plant(models.Model):
-    plant_name = models.CharField(max_length=100)
+    plant_name = models.CharField(max_length=100, unique=True)
     genus = models.CharField(max_length=100)
     image = models.CharField(max_length=100)
     origin = models.CharField(max_length=100)
