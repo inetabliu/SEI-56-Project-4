53cd58a496bba19e0d603faf872500cf9714df0c
Your Name <ineta.bliu@gmail.com>
2021-07-23 10:30:16 +0100

    plant mdoel created

--- the-plant-dispatch-app/plants/models.py
+++ the-plant-dispatch-app/plants/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Plant(models.Model):
     plant_name = models.CharField(max_length=100, unique=True)
     genus = models.CharField(max_length=100)
-    image = models.CharField(max_length=100, default="https://upload.wikimedia.org/wikipedia/commons/f/fe/Starr_080731-9572_Monstera_deliciosa.jpg")
+    image = models.CharField(max_length=100, default="https://res.cloudinary.com/inetab/image/upload/c_scale,h_400,w_600/v1627030331/SEI_Project_4/sblkzvbotq3ndadygtsy.jpg")
     origin = models.CharField(max_length=100)
 
     def __str__(self):
